578258c03cc7c8eef68f4e8046cb6ecda815df84
sarabarros-oli <saraholiveiraptu@gmail.com>
2025-10-27 12:06:57 -0300

    API do TCC

--- models.py
+++ models.py
@@ -2,6 +2,11 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from datetime import datetime, timezone
 from database import Base
+from datetime import datetime, timezone, timedelta
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class Usuario(Base):
     __tablename__ = "usuarios"
@@ -12,13 +17,21 @@ class Usuario(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     senha = Column(String, nullable=False)
 
+
 class Leitura(Base):
     __tablename__ = "leituras"
+
     id = Column(Integer, primary_key=True, index=True)
     ppm = Column(Integer, nullable=False)
     status = Column(String, nullable=False)
     origem = Column(String, nullable=True)
-    # use SEMPRE created_at
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    # e vincule ao usuário
+
+    # salva no horário de Brasília (GMT-3)
+    created_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone(timedelta(hours=-3))),
+        nullable=False
+    )
+
+    # vincula ao usuário
     user_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
