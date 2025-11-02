fa2e1bb485b7594a860f067acee4e5f775e75600
sarabarros-oli <saraholiveiraptu@gmail.com>
2025-11-02 20:03:58 -0300

    Atualizaçao banco

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
 
-DATABASE_URL ="postgresql://postgres:pkFRVPLzHLrvbkTICjbOqVwJJuBjJBUv@switchback.proxy.rlwy.net:49703/railway"
+DATABASE_URL ="postgresql://postgres:pkFRVPLzHLrvbkTICjbOqVwJJuBjJBUv@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
