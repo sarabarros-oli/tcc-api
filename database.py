ae257478642534c895abb5ae89634595934bed52
sarabarros-oli <saraholiveiraptu@gmail.com>
2025-11-02 20:44:09 -0300

    Atualizaçao banco

--- database.py
+++ database.py
@@ -2,11 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
-from database import Base, engine  # seu arquivo que tem engine e Base
-import models  # todos os modelos que você criou
-
-# Cria as tabelas no banco, caso não existam
-Base.metadata.create_all(bind=engine)
 
 
 DATABASE_URL ="postgresql://postgres:teDzZWkVFmoqSeIiDHtoiOnCeXmTmaDW@interchange.proxy.rlwy.net:23383/railway"
